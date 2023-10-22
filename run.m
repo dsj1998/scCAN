e82e5a9d789b42d1fad1c8dff0ab480095e8a43d
dsj1998 <93635787+dsj1998@users.noreply.github.com>
2023-10-22 20:47:06 +0800

    run.m

--- run.m
+++ run.m
@@ -1,36 +1,41 @@
-% This is a demo showing how to running CAN-Impute using the uso
-clear;clc
- addpath(genpath('./'))
-
+disp('--------------------start--------------------')
+tic
 %----------------------load mat processed data---------------------
 
-load('data.mat')
-data = Brain';
-data = log10(1+data);
-%----------------------------parameters-----------------------------
-K = 50;
-r = 20;
-lambda1 = 0.2;
-lambda2 = 0.4;
+data_sc = csvread('/data/liver.csv',1,1); 
+data_raw = data_sc';
+data = log10(1+data_raw);       
+
+cn = 7;                
+K = 7;
+r = 15;
+lambda1 = 2;
+lambda2 = 4;
+
+threshold = 0.1;
 paras = [K,r,lambda1,lambda2];
-
+                   
+                    
 %----------------------------find missing positions-----------------------------
-cn = 3;
-P = findMP(data,cn,threshold); % cn is the number of clusters 
-
-%--------------------------Initialization---------------------------
-[W0,H0,S0] = Initialize(data, paras,P, 100); % data;K;max_neighbors;lambda1;MaxIter
-Init = [{data},{W0},{H0},{S0},P];
+ P = findMP(data,cn,threshold);% cn is the number of clusters
+                    
+                    
+ %--------------------------Initialization---------------------------
+ [W0,H0,S0] = Initialize(data, paras,P,100); % data;K;max_neighbors;lambda1;MaxIter
+                     
+ Init = [{data},{W0},{H0},{S0},P];
+                    
 %----------------------------  run-----------------------------------
+[data_full,~,~,~] = adaptive_nmf(Init, paras);
 
-
-[data_full] = adaptive_nmf(Init, paras);% return the imputated data matrix;D is the imputed data
+                   
 X=data_full';
 Y= max(10.^X-1,0);
-
-fileDir_csv= 'result/';
-filename_mat='data.mat';
+                   
+fileDir_csv = 'result/'; 
+filename_csv="liver_result.csv";
 savePath_csv = strcat(fileDir_csv,filename_csv); 
 csvwrite(savePath_csv, Y);
 
-
+disp('--------------------end--------------------')
+addpath(genpath('./'))

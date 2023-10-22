b0c5c23b459d121924015054dfde8885c5a60734
dsj1998 <93635787+dsj1998@users.noreply.github.com>
2023-10-22 20:55:01 +0800

    Initialize.m

--- Initialize.m
+++ Initialize.m
@@ -11,22 +11,22 @@ rand('seed',0)
 Q = 1+rand(N, K);
 H = 1+rand(K, G);
 
-for i=1:MAXITER
+    for i=1:MAXITER
    
-    H = H .* (Q'*(P.*V))./(Q'*(P.*(Q*H))+ la1*H +eps);
-    Q = Q .* (P.*V*H')./((P.*(Q*H))*H' + la1*Q +eps);
+        H = H .* (Q'*(P.*V))./(Q'*(P.*(Q*H))+ la1*H +eps);
+        Q = Q .* (P.*V*H')./((P.*(Q*H))*H' + la1*Q +eps);
 
-end
+    end
 
 S = squareform(1-pdist(Q,'cosine'));
 
 B=zeros(size(S));
 
-for j=1:size(S,1)
-    [val,index] = maxk(S(j,:),r);
+    for j=1:size(S,1)
+        [val,index] = maxk(S(j,:),r);
     
-    B(j,index(1:r))=val(1:r);
-end
+        B(j,index(1:r))=val(1:r);
+    end
 S=B;
 S = max(S,S');
 end

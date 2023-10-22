25f597e06ac58d3574249091735e3f7482a5f9b4
dsj1998 <93635787+dsj1998@users.noreply.github.com>
2023-10-22 20:51:36 +0800

    adaptive_nmf.m

--- adaptive_nmf.m
+++ adaptive_nmf.m
@@ -1,55 +1,44 @@
-function [X] = adaptive_nmf(Init, paras)
-%Cosine distance
+function [X, Q, H, S] = adaptive_nmf(Init, paras)
 
 MAXITER = 1000;
 e = 1e-8;
-%---------------init--------------------------
 
+
+%---------------init--------------------------
 V = Init{1};
-W = Init{2};
+Q = Init{2};
 H = Init{3};
 S = Init{4};
 D = diag(sum(S));
-% P = zeros(size(V,1), size(V,2));
-% P(V > 0) = 1;
+
 P = Init{5};
 
 r = paras(2);
 la1 = paras(3);
 la2 = paras(4);
 %---------------------------------------------
-
-
+err = 1e-5;
 D1 = diag(sum(S).^-0.5);
-
-L0 = norm(P.*(V-W*H),'fro').^2 + la1.*(norm(W,'fro').^2 + norm(H,'fro').^2)+ la2.*trace(W'*(D1*(D-S)*D1)*W);
-  
+L0 = norm(P.*(V-Q*H),'fro').^2 + la1.*(norm(Q,'fro').^2 + norm(H,'fro').^2)+ la2.*trace(Q'*(D1*(D-S)*D1)*Q);
 for i=1:MAXITER
-    W = W .* (P.*V*H' + la2*D1*S*D1*W)./((P.*(W*H))*H' + la1*W + la2*D1*D*D1*W + eps);
-%     for j=1:size(W,1)
-%         W(j,:) = W(j,:)./norm(W(j,:));
-%     end
-    H = H .* (W'*(P.*V))./(W'*(P.*(W*H))+ la1*H +eps);
-         
-    L1 = norm(P.*(V-W*H),'fro').^2 + la1.*(norm(W,'fro').^2 + norm(H,'fro').^2)+ la2.*trace(W'*(D1*(D-S)*D1)*W);
-    LL = abs(L0-L1)/L1;
-            if LL<e
-                break
-            end
-    L0=L1;
-    Z(i) = L1;
+    Q = Q .* (P.*V*H' + la2*D1*S*D1*Q)./((P.*(Q*H))*H' + la1*Q + la2*D1*D*D1*Q + eps);
+
+    H = H .* (Q'*(P.*V))./(Q'*(P.*(Q*H))+ la1*H +eps);
     
+    error = mean(mean(abs(V-Q*H)))/mean(mean(V));
+
+     if error< err
+            break;
+    end     
     if i > (MAXITER/2)
-        
-        S = squareform(1-pdist(W,'cosine'));
-        
-        %S = squareform(1./(1+pdist(W,'seuclidean')));
-        
+        S = squareform(1-pdist(Q,'cosine'));
         B=zeros(size(S));
-        for j=1:size(S,1)
+        
+       for j=1:size(S,1)%
             [val,index] = maxk(S(j,:),r);
+           
             B(j,index(1:r))=val(1:r);
-            %B(j,index(1:r))=ones(1,r);
+          
         end
         S=B;    
         S = max(S,S');
@@ -59,7 +48,6 @@ for i=1:MAXITER
 
 end
 
-X = (1-P).*(W*H) + V;
+X = (1-P).*(Q*H) + V;
 
-plot(Z); 
 end
